e6b39735ef2379dba28e43554ba81faee19ce21a
unknown <itcdea02@iiht.tech>
2022-09-19 14:36:53 +0530

    second commit

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+7tyholpjg1wbxyffu)pkvvk3fk^8x*@6@o5hyg1o_yg1+7bv$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
